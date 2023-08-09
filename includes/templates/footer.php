af5e9bcd95e09e145c1aa20a7c2b565089faa17d
nancy <smithnancy@outlook.com>
2023-08-09 13:09:48 +1000

    including some tempates for header and footer

---
+++ includes/templates/footer.php
@@ -0,0 +1,11 @@
+<footer class="pt-5 mt-5 border-top">
+        <div class="container-xl mt-5">
+            <p class="mt-5 text-center fs-2">Rights reserved</p>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="/includes/js/bootstrap.bundle.min.js"></script>
+    <script src="/includes/js/app.js"></script>
+</body>
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lates News</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Raleway:wght@400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/includes/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/includes/css/app.css">
+</head>
+<body>
+    <header class="py-5 ">
+        <h1 class="text-center">
+            News
+            <span class="text-primary"> News</span>
+        </h1>
+    </header>
