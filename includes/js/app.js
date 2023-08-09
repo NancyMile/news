1e127b11632f6f5e1974e259093c0b0c778e5714
nancy <smithnancy@outlook.com>
2023-08-09 13:04:10 +1000

    Retriving the list of news and returning a json response

---
+++ includes/js/app.js
@@ -0,0 +1,23 @@
+
+
+const getData =
+$.get("http://localhost:8080/news.php", function (html) {
+    // retrieve list of news by article
+    const articleHTMLElements = $(html).find("article");
+
+    const articles = [];
+    // news with the scraped data
+    articleHTMLElements.each((i, articleHTML) => {
+        // scrape data from the article HTML element
+        const article = {
+        headLine: $(articleHTML).find("h4").text(),
+        articleSummary: $(articleHTML).find("p").text(),
+        url: $(articleHTML).find("a").attr("href"),
+        image: $(articleHTML).find("img").attr("src"),
+        };
+        articles.push(article);
+    });
+
+  console.log(JSON.stringify(articles));
+  return articles
+});
